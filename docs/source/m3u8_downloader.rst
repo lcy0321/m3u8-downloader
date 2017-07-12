c2185d8e706059fd671c280065c34e69a1e261eb
LCY <u900011@yahoo.com.tw>
2017-07-12 22:03:08 +0800

    Modify a lot of code

---
+++ docs/source/m3u8_downloader.rst
@@ -0,0 +1,6 @@
+m3u8\_downloader module
+=======================
+
+.. automodule:: m3u8_downloader
+    :members:
+    :exclude-members: main
--- m3u8_downloader.py
+++ m3u8_downloader.py
@@ -1,33 +1,47 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 """M3U8 Downloader
 
 Download the ts files according to the given m3u8 file.
 """
+import argparse
+import os
 
-import os.path
-from os import makedirs
-
+import ffmpy
 import m3u8
-from ffmpy import FFmpeg
-from requests import Session
 
-CHUNK_SIZE = 1024 * 1024    # 1MB
-"""int: The chunk size (in byte) used by requests when downloading from the
-server.
-"""
-FFMPEG_PATH = r'ffmpeg'
 
 class M3U8Downloader:
-    def __init__(self, *args, **kwargs):
-        self.uri = args[0]
-        self.m3u8 = m3u8.load(*args, **kwargs)
+    """M3U8 Downloader Class"""
 
-    def download(
-            self,
-            save_path='',
-            merge=True,
-            merged_file_name='merged.ts',
-            ts_list_name='ts_list.txt'):
+    def __init__(self, uri, timeout=None, headers=None,
+                 ffmpeg_path='ffmpeg', ffmpeg_loglevel='quiet'):
+        """Initialize a M3U8 Downloader.
+
+        Args:
+            uri (:obj:`str`): The URI of the m3u8 file.
+            timeout (:obj:`int`, optional): The timeout used when loading
+                from uri. Defaults to ``None``.
+            headers (:obj:`list` of :obj:`str`, optional): The headers used
+                when loading from uri. Defaults to ``None``.
+            ffmpeg_path (:obj:`str`, optional): The path to ffmpeg executable.
+                Defaults to ``ffmpeg``.
+            ffmpeg_loglevel (:obj:`str`, optional): The logging level of
+                ffmpeg. Defaults to ``quiet``.
+        """
+        if not headers:
+            headers = {}
+
+        self.uri = uri
+        self.ffmpeg_path = ffmpeg_path
+        self.ffmpeg_loglevel = ffmpeg_loglevel
+        self.m3u8 = m3u8.load(uri=uri, timeout=timeout, headers=headers)
+
+    def download(self, output='output.ts'):
+        """Start downloading and merging from the given m3u8 file.
+
+        Args:
+            output (:obj:`str`): The path to output. Defaults to ``output.ts``
+        """
         if self.m3u8.is_variant:
 
             print('There are multiple m3u8 file listed by this file.')
@@ -41,43 +55,29 @@ class M3U8Downloader:
                 fetch_index = int(input('Index> '))
 
                 dounloader = M3U8Downloader(
-                    self.m3u8.playlists[fetch_index].absolute_uri)
-                dounloader.download(
-                    save_path, merge, merged_file_name, ts_list_name)
+                    self.m3u8.playlists[fetch_index].absolute_uri,
+                    ffmpeg_path=self.ffmpeg_path,
+                    ffmpeg_loglevel=self.ffmpeg_loglevel,
+                )
+                dounloader.download(output)
             except (ValueError, IndexError):
                 print('Invalid index.')
 
         else:
-            makedirs(save_path, exist_ok=True)
-            session = Session()
+            dirname = os.path.dirname(output)
+            if dirname:
+                os.makedirs(os.path.dirname(output), exist_ok=True)
 
-            if merge:
-                merged_file_path = os.path.join(save_path, merged_file_name)
-                FFmpeg(FFMPEG_PATH,
-                       '-y',
-                       inputs={self.uri: None},
-                       outputs={merged_file_path: '-c copy'},
-                      ).run()
+            ffmpeg_cmd = ffmpy.FFmpeg(
+                self.ffmpeg_path,
+                '-y -loglevel {}'.format(self.ffmpeg_loglevel),
+                inputs={self.uri: None},
+                outputs={output: '-c copy'},
+            )
+            print('Start downloading and merging with ffmpeg...')
+            print(ffmpeg_cmd.cmd)
 
-            else:
-                ts_files = []
-                for segment in self.m3u8.segments:
-                    filename = os.path.basename(segment.uri)
-                    ts_file_path = os.path.join(save_path, filename)
-                    with open(ts_file_path, 'wb') as ts_file:
-                        self._download_and_write(session, segment, ts_file)
-                    ts_files.append(filename)
-
-                # Output the ts filenames
-                ts_list_path = os.path.join(save_path, ts_list_name)
-                with open(ts_list_path, 'w') as ts_list:
-                    ts_list.write('\n'.join(ts_files))
-
-    @classmethod
-    def _download_and_write(cls, session, segment, output_file):
-        req = session.get(segment.absolute_uri, stream=True)
-        for chunk in req.iter_content(chunk_size=CHUNK_SIZE):
-            output_file.write(chunk)
+            ffmpeg_cmd.run()
 
     @staticmethod
     def _print_stream_info(playlist, index=0):
@@ -98,10 +98,36 @@ class M3U8Downloader:
 
 
 def main():
-    downloader = M3U8Downloader('https://video.twimg.com/ext_tw_video/'
-                                '884699254991667201/pu/pl/'
-                                '6PuYiv2cXGlVZmB6.m3u8')
-    downloader.download('output')
+    """Parse the arguments to start the download.
+
+    Parse the arguments to construct the M3U8Downloader object, then start
+    downloading from the given m3u8 file.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('uri', help='URI of the m3u8 file')
+    parser.add_argument('-t', '--timeout', type=int, default=None,
+                        help='timeout used when loading from uri (default %(default)s)')
+    parser.add_argument('--ffmpeg-path', default='ffmpeg',
+                        help='path to ffmpeg executable (default %(default)s)')
+    parser.add_argument('--ffmpeg-loglevel', default='quiet',
+                        help='logging level of ffmpeg (default %(default)s)')
+    parser.add_argument('-o', '--output', default='output.ts',
+                        help='path to output (default %(default)s)')
+    parser.add_argument('-y', '--overwrite', action='store_true',
+                        help='overwrite output files without asking')
+    args = parser.parse_args()
+
+    if not args.overwrite and os.path.isfile(args.output):
+        print('ERROR: The file "{}" is already exist.'.format(args.output))
+        return
+
+    downloader = M3U8Downloader(
+        uri=args.uri,
+        timeout=args.timeout,
+        ffmpeg_path=args.ffmpeg_path,
+        ffmpeg_loglevel=args.ffmpeg_loglevel,
+    )
+    downloader.download(output=args.output)
 
 if __name__ == '__main__':
     main()
